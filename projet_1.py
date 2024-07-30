266d785ba2f056648603e5278246922847627927
audeflo <audendubuisi69@gmail.com>
2024-07-30 00:01:56 +0000

    nettoyage des fichiers

--- projet_1.py
+++ projet_1.py
@@ -6,76 +6,99 @@ import seaborn as sns
 import os
 
 
+
 # ----------------------IMPORTATION DES DONNEES------------------------------
+
 def importation(path):
     return pd.read_csv(path, encoding='ISO-8859-1')
 
 
 # customers
 print("TABLE CUSTOMERS")
-cst_path = "datasets/Customers.csv"
+cst_path = "./datasets/Customers.csv"
 customer = importation(cst_path)
+#print(customer.head())
 
 
 # data
 print("TABLE DATA")
 dt_path = "./datasets/Data_Dictionary.csv"
 data = importation(dt_path)
+#print(data.head())
 
 
 # exchange
 print("TABLE EXCHANGE")
 exc_path= "./datasets/Exchange_Rates.csv"
 exchange = importation(exc_path)
+#print(exchange.head())
 
 
 # product
 print("TABLE PRODUCT")
 prod_path= "./datasets/Products.csv"
 product = importation(prod_path)
+#print(product.head())
 
 
 # sales
 print("TABLE SALES")
 sls_path= "./datasets/Sales.csv"
 sales = importation(sls_path)
+#print(sales.head())
 
 
 # store
 print("TABLE STORE")
 str_path= "./datasets/Stores.csv"
 store = importation(str_path)
+#print(store.head())
 
 
 # ----------------------RENOMMER LES COLONNES------------------------------
 
+
 # customer
 customer= customer.rename(columns={"CustomerKey": "customer_id", "Gender": "gender", "Name": "name", "City":"cst_city","State Code": "cst_state_code", "State" : "cst_state","Zip Code":"cst_zip_code","Country" : "cst_country","Continent": "cst_continent","Birthday":"cst_birthday"})
+#print(customer.info())
+
 
 # data
 data = data.rename(columns={"Date" : "data_date","Currency" : "data_currency", "Exchange":"exchange"})
+#print(data.info())
+
 
 # exchange
 exchange = exchange.rename(columns={"Date": "exchange_date", "Currency":"currency_code", "Exchange":"exchange"})
+#print(exchange.info())
+
 
 # product
 product = product.rename(columns={"ProductKey": "product_id","Product Name": "product_name","Brand": "brand", "Color": "color", "Unit Cost USD": "unit_cost_USD", "Unit Price USD": "unit_price_USD", "SubcategoryKey": "subcategory_id","Subcategory": "subcategory", "CategoryKey": "category_id", "Category":"category"})
+#print(product.info())
+
 
 # sales
 sales = sales.rename(columns={"Order Number" : "order_number","Line Item" : "line_item", "Order Date":"order_date", "Delivery Date":"delivery_date", "CustomerKey": "customer_id" , "StoreKey":"store_id" ,"ProductKey": "product_id","Quantity":"quantity", "Currency Code":"currency_code"})
+#print(sales.info())
+
 
 # store
 store = store.rename(columns={"StoreKey":"store_id", "State":"store_state", "Square Meters":"square_meters", "Open Date":"open_date"})
+#print(store.info())
+
 
 
 # ----------------------INFORMATION SUR LES TYPES DE DONNEES------------------------------
 
-#print(customer.info())#
+#print(customer.info())
 #print(data.info())
 #print(exchange.info())
 #print(product.info())
 #print(sales.info())
 #print(store.info())
+
+
 
 # ----------------------MODIFICATION DES TYPES DE DONNEES NECESSAIRES------------------------------
 
@@ -86,13 +109,16 @@ customer['customer_id'] = customer['customer_id'].astype(str)
 customer['cst_birthday'] = pd.to_datetime(customer['cst_birthday'], format='%m/%d/%Y')
 
 #print(customer.info())
+#print(customer.head())
 
 
 # exchange
 exchange['exchange_date'] = pd.to_datetime(exchange['exchange_date'], format='%m/%d/%Y')
 
 #!!!!A FAIRE: transformer les "." en "," sans convertir le type de données dans la colonne exchange
+
 # print(exchange.info())
+# print(exchange.head())
 
 
 # product
@@ -109,6 +135,7 @@ product['subcategory_id'] = product['subcategory_id'].astype(str)
 product['category_id'] = product['category_id'].astype(str)
 
 #print(product.info())
+#print(product.head())
 
 
 # sales
@@ -121,6 +148,7 @@ sales['store_id'] = sales['store_id'].astype(str)
 sales['product_id'] = sales['product_id'].astype(str)
 
 #print(sales.info())
+#print(sales.head())
 
 
 # store
@@ -129,9 +157,12 @@ store['store_id'] = store['store_id'].astype(str)
 store['open_date'] = pd.to_datetime(store['open_date'], format='%m/%d/%Y')
 
 #print(store.info())
+#print(store.head())
+
 
 
 # ----------------------JOINTURE DES DATASETS------------------------------
+
 
 df_1 = pd.merge(sales, customer, how= "inner", on='customer_id')
 
@@ -139,4 +170,5 @@ df_2 = pd.merge(df_1, store,how= "inner", on='store_id')
 
 df = pd.merge(df_2, product,how= "inner", on='product_id')
 
-print(df.info())
+#print(df.info())
+#print(df.head())
