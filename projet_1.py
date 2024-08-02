ca848a73979a9c3a68876071348c5e9baa283715
salif437 <tsalif190@gmail.com>
2024-08-02 14:27:34 +0000

    amélioration

--- projet_1.py
+++ projet_1.py
@@ -10,7 +10,7 @@ import os
 
 # CUSTOMERS
 print("TABLE CUSTOMERS")
-cst_path = "C:/Users/SALIF/Desktop/projet_1/datasets/Customers.csv"
+cst_path = "./datasets/Customers.csv"
 customer_df = pd.read_csv(cst_path, encoding='ISO-8859-1')
 # print(customer.head())
 customers = customer_df.rename(columns={"CustomerKey": "customer_id", "Gender": "gender", "Name": "name", "State Code": "state_code", "State": "state", "Zip Code": "zip_code", "Continent": "continent","Birthday": "birthday"})
@@ -27,7 +27,7 @@ print(customer.isnull().any())
 
 # DATA
 print("TABLE DATA")
-dt_path = "C:/Users/SALIF/Desktop/projet_1/datasets/Data_Dictionary.csv"
+dt_path = "./datasets/Data_Dictionary.csv"
 data_df = pd.read_csv(dt_path, encoding='ISO-8859-1')
 # print(data.head())
 data = data_df.rename(columns={"Table": "table", "Field": "field", "Description": "description"})
@@ -38,7 +38,7 @@ print(data.isnull().any())
 
 # EXCHANGE
 print("TABLE EXCHANGE")
-exc_path= "C:/Users/SALIF/Desktop/projet_1/datasets/Exchange_Rates.csv"
+exc_path= "./datasets/Exchange_Rates.csv"
 exchange_df = pd.read_csv(exc_path, encoding='ISO-8859-1')
 # print(exchange.head())
 exchange = exchange_df.rename(columns={"Date": "date", "Currency": "currency", "Exchange": "exchange"})
@@ -49,7 +49,7 @@ print(exchange.isnull().any())
 
 # PRODUCT
 print("TABLE PRODUCT")
-prod_path= "C:/Users/SALIF/Desktop/projet_1/datasets/Products.csv"
+prod_path= "./datasets/Products.csv"
 product_df = pd.read_csv(prod_path, encoding='ISO-8859-1')
 # print(product.head())
 product = product_df.rename(columns={"ProductKey": "product_id", "Product Name": "Product_name", "Brand": "brand", "Color": "color", "Unit Cost USD": "unit_cost_USD", "Unit Price USD": "unit_price_USD", "SubcategoryKey": "subcategory_id","Subcategory": "subcategory", "CategoryKey": "Category_id", "Category":"category"})
@@ -61,7 +61,7 @@ print(product.isnull().any())
 
 # SALES
 print("TABLE SALES")
-sls_path= "C:/Users/SALIF/Desktop/projet_1/datasets/Sales.csv"
+sls_path= "./datasets/Sales.csv"
 sales_df = pd.read_csv(sls_path, encoding='ISO-8859-1')
 # print(sales.head())
 sales = sales_df.rename(columns={"Order Number": "order_id", "Line Item": "line_itm", "Order Date": "order_Date", "Delivery Date": "delivery_date", "CustomerKey":"customer_id", "StoreKey": "store_id","ProductKey": "product_id", "Quantity" : "qtity", "Currency Code" : "currency_id"})
@@ -79,7 +79,7 @@ print(sales.isnull().any())
 
 # STORE
 print("TABLE STORE")
-str_path= "C:/Users/SALIF/Desktop/projet_1/datasets/Stores.csv"
+str_path= "./datasets/Stores.csv"
 store_df = pd.read_csv(str_path, encoding='ISO-8859-1')
 # print(store.head())
 store = store_df.rename(columns={"StoreKey":"store_id","Country": "country","State" : "state","Square Meters": "square_meters","Open Date":"open_date"})
@@ -123,17 +123,18 @@ print(store.isnull().any())
 #                                              FUSION DATA
 
 # Fusionner les DataFrames par la colonne commune 
-df_1 = pd.merge(sales, customers, on='customer_id')
-df = pd.merge(df_1, product, on='product_id')
+df_1 = pd.merge(sales, customers, how= "outer", on='customer_id')
+df_2 = pd.merge(df_1, product,how= "outer", on='product_id')
+df = pd.merge(df_2, store,how= "outer", on='store_id')
 
 # Afficher le résultat
 print(df)
-df.head()
+print(df.head())
 #vérification de doublon dans le dataframe
 dup = df.duplicated().any()
-dup
+print(dup)
 #Changer le format des dates
 df['order_Date'] = pd.to_datetime(df['order_Date'], format='%m/%d/%Y')
 df['delivery_date'] = pd.to_datetime(df['delivery_date'], format='%m/%d/%Y')
 df['birthday'] = pd.to_datetime(df['birthday'], format='%m/%d/%Y')
-df
+print(df)
